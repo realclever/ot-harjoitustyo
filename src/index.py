5eb8bac4a72fecb7bd1a8d1716ea8f8f9c432583
Matti Lavikainen <matti.lavikainen@helsinki.fi>
2022-11-15 18:24:16 +0200

    project created

---
+++ src/index.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+window = Tk()
+window.title("Calculator application")
+window.mainloop()
+
---
+++ src/tests/__init__.py
@@ -0,0 +1,7 @@
+# Työaikakirjanpito
+
+| pvm | aika | mitä tein  |
+| :----:|:-----| :-----|
+| 14.11. | 1    | projektin luonti |
+| 15.11. | 2    | tutustuminen tkinteriin |
+| yht   | 3   | | 
